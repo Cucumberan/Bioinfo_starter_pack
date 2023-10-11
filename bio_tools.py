55346272f0292b64d8e761efa96ed6cfda3e0797
AO <anna.ogurtsova.chemmsu@gmail.com>
2023-10-12 02:12:00 +0300

    Add run_dna_rna_tools

--- bio_tools.py
+++ bio_tools.py
@@ -1,4 +1,6 @@
 from src.fastaq_functions import check_gc_content, check_length, check_quality
+from src.nucleic_acids_functions import check_valid_sequence, contains_T_and_U_at_the_same_time, transcribe, \
+    complement, reverse_complement, reverse
 
 
 def filter_fastq(seqs: dict, gc_bounds: tuple = (0, 100), length_bounds: tuple = (0, 2 ** 32),
@@ -34,5 +36,33 @@ def filter_fastq(seqs: dict, gc_bounds: tuple = (0, 100), length_bounds: tuple =
 
     return filtered_seqs
 
+
+def run_dna_rna_tools(*arguments):
+    """
+    Executes DNA/RNA sequence manipulation procedures.
+
+    Args:
+        *arguments (tuple): Variable-length argument list containing sequences and procedure.
+
+    Returns:
+        str or list of str: Result of the selected procedure.
+    """
+    procedure = arguments[-1]
+    sequences = arguments[:-1]
+    if not check_valid_sequence(sequences):
+        raise ValueError("At least one of your sequences does not correspond to either DNA or RNA")
+    if contains_T_and_U_at_the_same_time(sequences):
+        raise ValueError(
+            "One of your sequences contains both thymine and uracil at the same time, which is not possible((((")
+    if procedure == "transcribe":
+        return transcribe(sequences)
+    elif procedure == "reverse":
+        return reverse(sequences)
+    elif procedure == "complement":
+        return complement(sequences)
+    elif procedure == "reverse_complement":
+        return reverse_complement(sequences)
+    else:
+        return "Something went wrong, please, verify the chosen procedure is written correctly"
 
 
