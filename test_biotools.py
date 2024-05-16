2de172fcd46657314450c74190757c81a80eff1d
Cucumberan <anna.ogurtsova.chemmsu@gmail.com>
2024-05-16 13:58:20 +0400

    Add test TestConvertFasta

--- test_biotools.py
+++ test_biotools.py
@@ -1,6 +1,7 @@
 import unittest
 from biotools import DNASequence, RNASequence, check_quality
 from bio_files_processor import  OpenFasta, FastaRecord
+import os
 
 class TestBiotools(unittest.TestCase):
     def test_sequence_length(self):
@@ -65,6 +66,40 @@ class TestQualityCheck(unittest.TestCase):
         with self.assertRaises(ValueError):
             check_quality(quality_scores_dict, 20)
 
+
+class TestConvertFasta(unittest.TestCase):
+    """
+    A unit test class for testing the convert_multiline_fasta_to_oneline function from the bio_files_processor module.
+
+    This test class ensures that the conversion from a multiline FASTA format to a single-line FASTA format is performed correctly. The class uses the unittest framework to manage setup, execution, and teardown of tests.
+
+    Methods:
+        setUp(self): Prepares the environment before each test function is executed. It creates a sample multiline FASTA file to be used in the tests.
+        test_conversion(self): Executes the test to verify that the convert_multiline_fasta_to_oneline function correctly converts a given multiline FASTA file to the expected single-line format. The test checks that the content of the converted file matches the expected results.
+        tearDown(self): Cleans up the environment after each test function has been executed. It removes any files created during the test setup to ensure a clean state for subsequent tests.
+
+    Usage:
+        Running this test class checks the functionality of the convert_multiline_fasta_to_oneline function to ensure it accurately handles the conversion process without data loss or format errors.
+    """
+    def setUp(self):
+        self.input_fasta = "test.fasta"
+        self.output_fasta = "test_one_line.fasta"
+        with open(self.input_fasta, 'w') as f:
+            f.write(">seq1\nATGCA\nTGACG\n>seq2\nTTGAC\nGTACA\n")
+
+    def test_conversion(self):
+        # Import the function to test
+        from bio_files_processor import convert_multiline_fasta_to_oneline
+        convert_multiline_fasta_to_oneline(self.input_fasta, self.output_fasta)
+        expected_results = ">seq1\nATGCATGACG\n>seq2\nTTGACGTACA\n"
+        with open(self.output_fasta, 'r') as f:
+            results = f.read()
+        self.assertEqual(results, expected_results)
+
+    def tearDown(self):
+        # Clean up: Remove files created for the test
+        os.remove(self.input_fasta)
+        os.remove(self.output_fasta)
 
 if __name__ == '__main__':
     unittest.main()
